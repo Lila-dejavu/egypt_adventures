d15cfa4853c2a1aa2c44601cef7cba8bf6db9c35
Lila <lila.bahuet@gmail.com>
2025-12-06 16:02:00 +0100

    chore: 更新版本號至 1.1.38

--- js/core/App.js
+++ js/core/App.js
@@ -8,7 +8,7 @@ const App = {
 
     // Initialize App
     init() {
-        this.version = DOMRefs.versionDisplay ? DOMRefs.versionDisplay.textContent.trim() : 'Version 1.1.37';
+        this.version = DOMRefs.versionDisplay ? DOMRefs.versionDisplay.textContent.trim() : 'Version 1.1.38';
         console.log('App Version:', this.version);
     },
 
