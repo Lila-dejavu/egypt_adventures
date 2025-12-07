57a2e6c694bc8762799ae0e0f36359a70b7969b2
Lila <lila.bahuet@gmail.com>
2025-12-07 23:22:51 +0100

    fix(version): 更新遊戲版本號至 1.1.45

--- js/core/App.js
+++ js/core/App.js
@@ -8,7 +8,7 @@ const App = {
 
     // Initialize App
     init() {
-        this.version = DOMRefs.versionDisplay ? DOMRefs.versionDisplay.textContent.trim() : 'Version 1.1.44';
+        this.version = DOMRefs.versionDisplay ? DOMRefs.versionDisplay.textContent.trim() : 'Version 1.1.45';
         console.log('App Version:', this.version);
     },
 
