457a761d123c5d9f89075ecdcc7ae58312d7d97e
Lila <lila.bahuet@gmail.com>
2025-12-07 23:40:05 +0100

    fix(version): 更新遊戲版本號至 1.1.47，調整樣式以隱藏小螢幕上的象形文字裝飾

--- js/core/App.js
+++ js/core/App.js
@@ -8,7 +8,7 @@ const App = {
 
     // Initialize App
     init() {
-        this.version = DOMRefs.versionDisplay ? DOMRefs.versionDisplay.textContent.trim() : 'Version 1.1.46';
+        this.version = DOMRefs.versionDisplay ? DOMRefs.versionDisplay.textContent.trim() : 'Version 1.1.47';
         console.log('App Version:', this.version);
     },
 
